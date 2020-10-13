0576a68ea48a2e322f3d978f83a69117fdf7c96f
Mark McClean <mcclean_mark_02@hotmail.com>
2020-10-13 19:17:45 +0000

    Created a routes view, the route url and a basic template to test if the view is rendered

--- routes/admin.py
+++ routes/admin.py
@@ -3,6 +3,7 @@ from .models import Route, BikeType
 
 # Register your models here.
 
+
 class RouteAdmin(admin.ModelAdmin):
     list_display = (
         'name',
@@ -17,11 +18,15 @@ class RouteAdmin(admin.ModelAdmin):
 
     ordering = ('bike_type',)
 
+
 class BikeTypeAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
     )
 
+
 admin.site.register(Route, RouteAdmin)
+
+
 admin.site.register(BikeType, BikeTypeAdmin)
--- routes/views.py
+++ routes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Route
 
 # Create your views here.
+
+
+def all_routes(request):
+    """ A view to show all routes, including sorting and search queries """
+
+    routes = Route.objects.all()
+
+    context = {
+        'routes': routes,
+    }
+    return render(request, 'routes/routes.html', context)
