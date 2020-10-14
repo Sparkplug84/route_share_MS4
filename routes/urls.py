c33e1b1a3ce247afaab62ee6b00204b44c7569b7
Mark McClean <mcclean_mark_02@hotmail.com>
2020-10-14 21:13:13 +0000

    Started route details view, connecting the url and starting the basic template

--- routes/urls.py
+++ routes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_routes, name='routes')
+    path('', views.all_routes, name='routes'),
+    path('<int:route_id>/', views.route_detail, name='route_detail'),
 ]
--- routes/views.py
+++ routes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Route
 
 # Create your views here.
@@ -13,3 +13,14 @@ def all_routes(request):
         'routes': routes,
     }
     return render(request, 'routes/routes.html', context)
+
+
+def route_detail(request, route_id):
+    """ A view to show individual route details """
+
+    route = get_object_or_404(Route, pk=route_id)
+
+    context = {
+        'route': route,
+    }
+    return render(request, 'routes/route_detail.html', context)
