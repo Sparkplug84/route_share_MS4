2ed973d70777c1f2fc95f5e56daf751c6fc496c2
Mark McClean <mcclean_mark_02@hotmail.com>
2020-10-15 21:08:48 +0000

    styling adjustments to the routes and route details and added search box functionality

--- routes/models.py
+++ routes/models.py
@@ -14,12 +14,19 @@ class BikeType(models.Model):
         return self.friendly_name
 
 
+route_types = [
+    ('', 'Select Route Type'),
+    ('One Way', 'One Way'),
+    ('Round Trip', 'Round Trip')
+]
+
+
 class Route(models.Model):
     bike_type = models.ForeignKey('BikeType', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
     length = models.DecimalField(max_digits=4, decimal_places=1)
-    route_type = models.CharField(max_length=254)
+    route_type = models.CharField(max_length=254, choices=route_types)
     map_url = models.URLField(max_length=1054)
     country = CountryField(blank_label='Country *')
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
--- routes/views.py
+++ routes/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Route
 
 # Create your views here.
@@ -8,9 +10,21 @@ def all_routes(request):
     """ A view to show all routes, including sorting and search queries """
 
     routes = Route.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error("You didn't enter any search criteria")
+                return redirect(reverse('routes'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(country__icontains=query) | Q(route_type__icontains=query) | Q(length__icontains=query) 
+            routes = routes.filter(queries)
 
     context = {
         'routes': routes,
+        'search_term': query,
     }
     return render(request, 'routes/routes.html', context)
 
