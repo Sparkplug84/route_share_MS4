ee4c39e8c9c7c3cf860ef636a7071ae5e508101e
Mark McClean <mcclean_mark_02@hotmail.com>
2020-10-22 20:54:38 +0000

    Updated basket views and contexts to enable adding a membership type to the basket.

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+from membership.models import Membership
 
 
 def basket_contents(request):
@@ -5,6 +7,17 @@ def basket_contents(request):
     basket_items = []
     total = 0
     basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        membership = get_object_or_404(Membership, pk=item_id)
+        total += quantity * membership.price
+        basket_items.append(
+            {
+                'item_id': item_id,
+                'quantity': quantity,
+                'membership': membership,
+            }
+        )
 
     context = {
         'basket_items': basket_items,
--- basket/views.py
+++ basket/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
 
 def view_basket(request):
     """ A view to return the basket contents """
     return render(request, 'basket/basket.html')
+
+
+def add_to_basket(request, item_id):
+    """ A view to add a membership type to the basket """
+
+    membership_qty = 1
+    basket = request.session.get('basket', {})
+
+    if request.user.is_authenticated:
+        if basket.items():
+            messages.error(request, "You've already added a Membership Plan to your basket.")
+            return redirect('view_basket')
+
+        basket[item_id] = basket.get(item_id, membership_qty)
+        request.session['basket'] = basket
+
+        messages.success(request, "You've added the Membership Plan to your basket!")
+        print(request.session['basket'])
+        return redirect(reverse('membership'))
+
+    messages.error(request, "You need to be signed in to add a Membership Plan to the basket.")
+    return redirect('account_signup')
