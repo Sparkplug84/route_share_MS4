f00a8d089f482d89bf1d1c375fa8a007af71a6bd
Mark McClean <mcclean_mark_02@hotmail.com>
2020-10-26 15:16:17 +0000

    Updated order model and removed orderlineitem model. Added form

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_number', 'date', 'order_total',)
+
+    list_display = ('order_number', 'full_name', 'date',
+                    'order_total',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
--- checkout/models.py
+++ checkout/models.py
@@ -1,7 +1,7 @@
 import uuid
 from django.db import models
-from django.contrib.auth.models import User
 from membership.models import Membership
+from django_countries.fields import CountryField
 
 
 class Order(models.Model):
@@ -9,38 +9,30 @@ class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.CharField(max_length=250, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
+    membership = models.ForeignKey(
+        Membership, null=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, default=1, editable=False)
     order_total = models.DecimalField(
-        max_digits=10, decimal_places=2, null=False, default=0)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+        max_digits=6, decimal_places=2, null=False,
+        blank=False, editable=False)
 
     def _generate_order_number(self):
         """ Generate a unique random order number """
         return uuid.uuid4().hex.upper()
 
     def save(self, *args, **kwargs):
-        """ Override the original save order method
-        to set the order numer if it hasn't been set yet """
+        """ Overrides the original save method to set the the order total """
+        self.order_total = self.membership.price
         if not self.order_number:
             self.order_number = self._generate_order_number()
-        super().save()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.order_number
-
-
-class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, on_delete=models.CASCADE)
-    membership = models.ForeignKey(
-        Membership, null=False, on_delete=models.CASCADE)
-    quantity = models.IntegerField(null=False, default=1)
-
-    def __str__(self):
-        return f'{self.quantity} {self.membership.name} ' \
-            'on order {order.order_number}'
