0228d03a95ef2bc62655b154d540e3c550b3bd57
Mark McClean <mcclean_mark_02@hotmail.com>
2020-11-09 21:47:50 +0000

    Updated checkout view to include current user info in form

--- checkout/admin.py
+++ checkout/admin.py
@@ -3,10 +3,11 @@ from .models import Order
 
 
 class OrderAdmin(admin.ModelAdmin):
-    readonly_fields = ('order_number', 'date', 'order_total', 'original_basket', 'stripe_pid')
+    readonly_fields = ('order_number', 'date',
+                       'order_total', 'original_basket', 'stripe_pid')
 
     list_display = ('order_number', 'full_name', 'date',
-                    'order_total',)
+                    'order_total', 'user_profile')
 
     ordering = ('-date',)
 
--- checkout/views.py
+++ checkout/views.py
@@ -7,6 +7,8 @@ from django.conf import settings
 from .forms import OrderForm
 from basket.contexts import basket_contents
 from membership.models import Membership
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 from .models import Order
 
 import stripe
@@ -84,7 +86,22 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        order_form = OrderForm()
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name,
+                    'email': profile.user.email,
+                    'street_address1': profile.default_street_address1,
+                    'street_address2': profile.default_street_address2,
+                    'town_or_city': profile.default_town_or_city,
+                    'postcode': profile.default_postcode,
+                    'country': profile.default_country,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
@@ -104,6 +121,26 @@ def checkout_success(request, order_number):
     """ Handle successful checkouts """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    profile = UserProfile.objects.get(user=request.user)
+    # Attach the users profile to the order
+    order.user_profile = profile
+    order.save()
+
+    if save_info:
+        profile_data = {
+            'default_full_name': order.full_name,
+            'default_email': order.email,
+            'default_street_address1': order.street_address1,
+            'default_street_address2': order.street_address2,
+            'default_town_or_city': order.town_or_city,
+            'default_postcode': order.postcode,
+            'default_country': order.country,
+        }
+        user_profile_form = UserProfileForm(profile_data, instance=profile)
+        if user_profile_form.is_valid():
+            user_profile_form.save()
+
     messages.success(request, f'Order successfully processed. \
         Your order number is {order_number}. A confirmation \
             email has been sent to {order.email}.')
