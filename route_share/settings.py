c123b96d10b244ce619eccd571046b0ebf43c82c
Mark McClean <mcclean_mark_02@hotmail.com>
2020-11-15 20:50:19 +0000

    New secret key and debug settings

--- route_share/settings.py
+++ route_share/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jo)m9!)2jq4mr-937x6*+@wynehzp!y@^de9ij4zln*ec2ep*b'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['bicycle-route-share.herokuapp.com', 'localhost']
 
