e785cb9c8c13753c8b6c79ea5565a1827e1041a8
Mark McClean <mcclean_mark_02@hotmail.com>
2020-11-15 20:32:43 +0000

    Further Heroku deployment settings initialized

--- route_share/settings.py
+++ route_share/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'jo)m9!)2jq4mr-937x6*+@wynehzp!y@^de9ij4zln*ec2ep*b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bicycle-route-share.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +123,17 @@ WSGI_APPLICATION = 'route_share.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
