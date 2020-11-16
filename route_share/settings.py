2c7ddf9e131b4c08a1c12a12f23b5ce98fe469df
Mark McClean <mcclean_mark_02@hotmail.com>
2020-11-16 08:06:34 +0000

    Updated settings for static/media files

--- route_share/settings.py
+++ route_share/settings.py
@@ -179,7 +179,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if 'USE-AWS' in os.environ:
+if 'USE_AWS' in os.environ:
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'bicycle-route-share'
     AWS_S3_REGION_NAME = 'us-east-1'
