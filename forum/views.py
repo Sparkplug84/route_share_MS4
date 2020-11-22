b165476c9fe64b59c4b5789d8b635b4ae97b30f4
Mark McClean <mcclean_mark_02@hotmail.com>
2020-11-22 19:02:03 +0000

    New posts can now be added to the forum

--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.contrib import messages
 from .models import ForumPost, ForumPostReply
 from .forms import ForumForm
 
@@ -9,13 +10,27 @@ def forum(request):
 
     context = {
         'forum_posts': forum_posts,
+        'on_forum_page': True
     }
     return render(request, 'forum/forum.html', context)
 
 
 def add_post(request):
     """ A view to add a new post to the forum page """
-    form = ForumForm()
+    if request.method == 'POST':
+        form = ForumForm(request.POST)
+        user = request.user
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.post_user = user
+            post.save()
+            messages.success(request, 'You have added a new forum post!')
+            return redirect(reverse('forum'))
+        else:
+            messages.error(request, 'Failed to add new post. \
+                Please ensure the form is valid.')
+    else:
+        form = ForumForm()
     template = 'forum/add_post.html'
     context = {
         'form': form,
