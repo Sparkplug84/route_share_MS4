d2493b7f2496864f7fdc95db2ae1fc5468b0a39b
Mark McClean <mcclean_mark_02@hotmail.com>
2020-11-22 15:50:40 +0000

    Add post tamplate, view and form created

---
+++ forum/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import ForumPost, ForumPostReply
+
+
+class ForumForm(forms.ModelForm):
+
+    class Meta:
+        model = ForumPost
+        fields = [
+            'title',
+            'post',
+        ]
+
+    def __init__(self, *args, **kwargs):
+        """ Getting foreign key friendly names and setting paceholder names """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'title': 'Post Title',
+            'post': 'Your Post',
+        }
+
+        self.fields['title'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
--- forum/views.py
+++ forum/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from .models import ForumPost, ForumPostReply
+from .forms import ForumForm
 
 
 def forum(request):
     """ A view to return index page """
-    forum_posts = ForumPost.objects.all()
+    forum_posts = ForumPost.objects.order_by('-post_date')
 
     context = {
         'forum_posts': forum_posts,
     }
     return render(request, 'forum/forum.html', context)
+
+
+def add_post(request):
+    """ A view to add a new post to the forum page """
+    form = ForumForm()
+    template = 'forum/add_post.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
