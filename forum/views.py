e6fa3f705f6f7c10bc896f474e63ad5d4611295a
Mark McClean <mcclean_mark_02@hotmail.com>
2020-11-22 10:01:00 +0000

    Template started to render forum posts

--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import ForumPost, ForumPostReply
 
 
 def forum(request):
     """ A view to return index page """
-    return render(request, 'forum/forum.html')
+    forum_posts = ForumPost.objects.all()
+
+    context = {
+        'forum_posts': forum_posts,
+    }
+    return render(request, 'forum/forum.html', context)
