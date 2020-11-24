af107895ad4eff3bec668466ba2ddc6bfa085a6d
Mark McClean <mcclean_mark_02@hotmail.com>
2020-11-24 19:27:52 +0000

    Added 404 error page

--- route_share/settings.py
+++ route_share/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['bicycle-route-share.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['bicycle-route-share.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
