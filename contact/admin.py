3596df59665ba3568bd344331682d39ba46cc71b
Mark McClean <mcclean_mark_02@hotmail.com>
2020-11-25 10:19:17 +0000

    Contact form post operational and creates admin record

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+# Registered Models for Admin
+admin.site.register(Contact)
--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .forms import ContactForm
 
 
-def contact(request):  
+def contact(request):
     """ A view to render the contact form """
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'You have sent a message to the Route Share Team. \
+                             We will reply as soon as possible.')
+        else:
+            messages.error(request, 'Failed to send message. \
+                Please ensure the form is valid.')
+
     form = ContactForm()
     template = 'contact/contact.html'
     context = {
         'form': form,
     }
     return render(request, template, context)
-
